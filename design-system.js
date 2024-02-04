3487ed4588d8115994508989a427d13710cb6b88
John <john24alex@yahoo.com>
2024-02-04 18:37:06 -0500

    Add mobile menu toggle function

---
+++ design-system.js
@@ -0,0 +1,11 @@
+
+// Mobile menu toggle function
+function toggleMobileMenu() { 
+    var menu = document.querySelector('.navbar__side');
+    var toggle = document.querySelector('.navbar__mobile-toggle');
+    
+    menu.classList.toggle('navbar__side--open');
+    toggle.classList.toggle('open');
+}
+
+
