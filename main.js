54a91b26c7f45add6eeb3cdc321603038b22170c
John <john24alex@yahoo.com>
2024-03-26 21:37:26 -0500

    Add slider

--- main.js
+++ main.js
@@ -9,3 +9,24 @@ function toggleMobileMenu() {
 }
 
 
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function nextSlide() {
+  if (currentSlide < totalSlides - 1) {
+    currentSlide++;
+  } else {
+    currentSlide = 0;
+  }
+  updateSlidePosition();
+}
+
+function updateSlidePosition() {
+  const slider = document.querySelector('.slider');
+  const slideWidth = slides[currentSlide].clientWidth;
+  const newTransformValue = -(slideWidth * currentSlide);
+  slider.style.transform = `translateX(${newTransformValue}px)`;
+}
+
+//setInterval(nextSlide, 3000); // Cambia el slide cada 3 segundos
